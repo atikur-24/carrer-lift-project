bc890e0e69a49dc3704d9b6e5490c7cd77a256a6
Atikur <atikurrahman7223@gmail.com>
2023-04-12 16:10:23 +0600

    responsive detail page content

--- src/components/Home/Features.jsx
+++ src/components/Home/Features.jsx
@@ -12,14 +12,14 @@ const Features = ({ feature }) => {
                 <img src={logo} alt="logo"  className='object-cover'/>
             </div>
             <div className='space-y-5'>
-                <h2 className='text-2xl tracking-wide font-extrabold text-[#474747]'>{job_title}</h2>
+                <h2 className='text-xl md:text-2xl tracking-wide font-extrabold text-[#474747]'>{job_title}</h2>
                 <p className='text-xl font-semibold text-[#757575]'>{brand_name}</p>
-                <p className='text-indigo-400 font-bold'>
-                    <span className='rounded-md px-2 py-3 text-center mr-4 border border-indigo-400'>{office}</span> <span className='rounded-lg px-2 py-3 text-center border  border-indigo-400'>{job_type}</span>
+                <p className='text-indigo-400 font-medium md:font-bold'>
+                    <span className='rounded-md px-1 py-2 md:px-2 md:py-3 text-center mr-4 border border-indigo-400'>{office}</span> <span className='rounded-lg px-1 md:px-2 py-2 md:py-3 text-center border  border-indigo-400'>{job_type}</span>
                 </p>
                 <div>
-                    <p className='inline-flex items-center text-[#757575] gap-2 mr-6 text-xl'> <span> <MapPinIcon className='h-6 w-5' /> </span> {location} </p> 
-                    <p className='inline-flex items-center text-[#757575] gap-2 text-xl'> <span> <CurrencyDollarIcon className='h-6 w-5' /> </span> Salary: {salary} </p> 
+                    <p className='inline-flex items-center text-[#757575] gap-2 mr-6 md:text-xl'> <span> <MapPinIcon className='h-6 w-5' /> </span> {location} </p> 
+                    <p className='inline-flex items-center text-[#757575] gap-2 md:text-xl'> <span> <CurrencyDollarIcon className='h-6 w-5' /> </span> Salary: {salary} </p> 
                 </div>
                 <button onClick={ () => navigate(`details/${id}`) } className='btn md:h-12'>View Details</button>
             </div>
